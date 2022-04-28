1befb7044ecbefd19a88c01ab502f5e934d7bcc1
Rajan Chaudhary <60035342+rajann44@users.noreply.github.com>
2022-04-29 00:29:33 +0530

    updated github test

--- cypress/integration/github.spec.js
+++ cypress/integration/github.spec.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
 
 describe('Visit Version Control', () => {
-    it('Visits Github', () => {
+    it('Visits Github twice', () => {
+      cy.visit('https://github.com')
       cy.visit('https://github.com')
       expect(true).to.equal(true)
     })
